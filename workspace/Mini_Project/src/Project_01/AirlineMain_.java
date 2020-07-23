5feb3fec45bd011bb5c99eb34e8bc91d336cd0ab
changman <changman2147@gmail.com>
2020-07-23 20:19:24 +0900

    2020-07-23

--- workspace/Mini_Project/src/Project_01/AirlineMain_.java
+++
@@ -1,8 +0,0 @@
-package Project_01;
-
-public class AirlineMain_ {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new AirlineMain_Login();
-	}
-}
--- workspace/Mini_Project/src/Project_01/Member_.java
+++
@@ -1,7 +0,0 @@
-package Project_01;
-
-public class Member_ {
-	public Member_() {
-       new Member_BookInfo().event();
-	}
-}
